5e45cce24706f97e6b9992e1f1d17a812f4026f7
Eduard Rius Martinez <eduard.rius@ross-robotics.co.uk>
2024-10-24 09:41:42 +0100

    Modify blink example

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -1,48 +1,36 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <stdio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+/* Define the nodes corresponding to the LEDs */
+#define LED0_NODE DT_NODELABEL(led0)
+#define LED1_NODE DT_NODELABEL(led1)
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+/* Get the GPIO device specifications for both LEDs */
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-int main(void)
+void main(void)
 {
-	int ret;
-	bool led_state = true;
+    if (!device_is_ready(led0.port) || !device_is_ready(led1.port)) {
+        return;
+    }
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+    gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+    gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
+    while (1) {
+        /* Toggle LED0 on, LED1 off */
+        gpio_pin_set_dt(&led0, 1);
+        gpio_pin_set_dt(&led1, 0);
+        k_msleep(1000); /* Sleep for 1000 ms */
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+        /* Toggle LED0 off, LED1 on */
+        gpio_pin_set_dt(&led0, 0);
+        gpio_pin_set_dt(&led1, 1);
+        k_msleep(1000); /* Sleep for 1000 ms */
+    }
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
-	}
-	return 0;
+    return 0;
 }
